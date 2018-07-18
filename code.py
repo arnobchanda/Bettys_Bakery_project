7f23dcec89459eaac8061694a724390ed989c692
Arnob Chanda <38960520+arnobchanda@users.noreply.github.com>
2018-07-18 15:04:26 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+cupcakes = np.array([2,0.75,2,1,0.5])
+recipes = np.genfromtxt("recipes.csv",delimiter=',')
+print(recipes)
+
+eggs = recipes[:,2]
+print(eggs)
+
+one_egg = eggs == 1
+print(one_egg)
+
+cookies = recipes[2,:]
+print(cookies)
+
+double_batch = cupcakes * 2
+print(double_batch)
+
+grocery_list = np.array([cookies,double_batch])
+print(grocery_list)
